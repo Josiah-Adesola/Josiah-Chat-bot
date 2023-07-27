f8dfb9be6f2cb13fea44f74fc28f43836bab77ff
Josiah Adesola <59745353+Josiah-Adesola@users.noreply.github.com>
2023-07-27 08:27:10 +0000

    added to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+from hugchat import hugchat
+from hugchat.login import Login
 
-st.title('🎈 App Name')
+st.title('🎈 Josiah-Chatbot')
+st.set_page_config(page_title="🤗💬 Josiah-Chatbot")
+
 
 st.write('Hello world!')
